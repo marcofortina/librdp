a5b50d8244cf2bddd323575a9dd9673c5dba784e
Marco Fortina <marco_fortina@hotmail.it>
2026-07-12 12:30:17 +0200

    Add optional backend build gate

---
+++ tests/core_optional_backends_off.cmake
@@ -0,0 +1,64 @@
+# Copyright (C) 2026 Marco Fortina
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+if(NOT DEFINED LIBRDP_SOURCE_DIR)
+    message(FATAL_ERROR "LIBRDP_SOURCE_DIR is required")
+endif()
+if(NOT DEFINED LIBRDP_BINARY_DIR)
+    message(FATAL_ERROR "LIBRDP_BINARY_DIR is required")
+endif()
+
+set(optional_off_binary_dir "${LIBRDP_BINARY_DIR}/core-optional-backends-off")
+file(REMOVE_RECURSE "${optional_off_binary_dir}")
+
+set(configure_args
+    -S "${LIBRDP_SOURCE_DIR}"
+    -B "${optional_off_binary_dir}"
+    -DLIBRDP_BUILD_TESTS=ON
+    -DLIBRDP_BUILD_FUZZ=OFF
+    -DLIBRDP_BUILD_X11_VIEWER=OFF
+    -DLIBRDP_BUILD_EXAMPLES=OFF
+    -DLIBRDP_WITH_FFMPEG_AVC=OFF
+    -DLIBRDP_WITH_OPENH264_AVC=OFF
+    -DLIBRDP_WITH_PCSC=OFF
+    -DLIBRDP_WITH_LIBUSB=OFF
+    -DLIBRDP_WITH_FIDO2=OFF
+    -DLIBRDP_WITH_CBOR=OFF
+    -DLIBRDP_WITH_CUPS=OFF
+    -DLIBRDP_WITH_ACL=OFF
+    -DLIBRDP_WITH_ATTR=OFF
+    -DLIBRDP_WITH_ARCHIVE=OFF
+    -DLIBRDP_WITH_PIPEWIRE=OFF
+    -DLIBRDP_WITH_JPEG=OFF
+)
+
+if(DEFINED LIBRDP_CMAKE_GENERATOR AND NOT "${LIBRDP_CMAKE_GENERATOR}" STREQUAL "")
+    list(APPEND configure_args -G "${LIBRDP_CMAKE_GENERATOR}")
+endif()
+if(DEFINED LIBRDP_C_COMPILER AND NOT "${LIBRDP_C_COMPILER}" STREQUAL "")
+    list(APPEND configure_args -DCMAKE_C_COMPILER=${LIBRDP_C_COMPILER})
+endif()
+if(DEFINED LIBRDP_BUILD_TYPE AND NOT "${LIBRDP_BUILD_TYPE}" STREQUAL "")
+    list(APPEND configure_args -DCMAKE_BUILD_TYPE=${LIBRDP_BUILD_TYPE})
+endif()
+
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" ${configure_args}
+    RESULT_VARIABLE configure_result
+)
+if(NOT configure_result EQUAL 0)
+    message(FATAL_ERROR "optional backend OFF configure failed with ${configure_result}")
+endif()
+
+set(build_args --build "${optional_off_binary_dir}" --target librdp test_core)
+if(DEFINED LIBRDP_BUILD_CONFIG AND NOT "${LIBRDP_BUILD_CONFIG}" STREQUAL "")
+    list(APPEND build_args --config "${LIBRDP_BUILD_CONFIG}")
+endif()
+
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" ${build_args}
+    RESULT_VARIABLE build_result
+)
+if(NOT build_result EQUAL 0)
+    message(FATAL_ERROR "optional backend OFF build failed with ${build_result}")
+endif()
